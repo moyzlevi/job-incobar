da1f1580b397d85f250204d14d66e127d5f472f4
moyses-santos <moyzlevi@gmail.com>
2021-05-10 17:39:32 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css'
+import {Button} from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+    return (<div className="container">
+       <Button>Hello World</Button>
+    </div>)
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // Where files should be sent once they are bundled
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.bundle.js",
+  },
+  // webpack 5 comes with devServer which loads in development mode
+  devServer: {
+    port: 3000,
+    watchContentBase: true,
+    open: true,
+  },
+  // Rules of how webpack will take our files, complie & bundle them for the browser
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./static/index.html",
+      favicon: "./static/favicon.ico",
+    }),
+  ],
+
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
+  },
+};
