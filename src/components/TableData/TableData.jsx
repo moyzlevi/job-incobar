a91618cbc6441e9cd01502e19e57aa0f2c828680
moyses-santos <moyzlevi@gmail.com>
2021-05-15 10:20:58 -0300

    feat:finished column formatting

--- src/components/TableData/TableData.jsx
+++ src/components/TableData/TableData.jsx
@@ -1,57 +1,67 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./TableData.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Register from "../Register/Register";
 
 import getData from "./service.js";
 
-export default class TableData extends Component{
-  constructor(props){
+export default class TableData extends Component {
+  constructor(props) {
     super(props);
-    this.state = {list:[]}
+    this.state = { list: [] };
     // this.refresh.bind = this.refresh.bind(this);
   }
-  
-  async componentDidMount(){
+
+  async componentDidMount() {
     this.refresh();
   }
-  
-  printState(){
-    let myState = this.state.list
+
+  printState() {
+    let myState = this.state.list;
     console.log(myState);
   }
 
-  refresh(){
+  refresh() {
     let data = getData().operations;
     this.setState({
-      list: data
-    })
+      list: data,
+    });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-      <div className="TableData text-font-medium center-text">
-        <div className="TableData__head--data ">Data</div>
-        <div className="TableData__head--produto">Produto</div>
-        <div className="TableData__head">Un.</div>
-        <div className="TableData__head">Qtd.</div>
-        <div className="TableData__head">Qtd Prod.</div>
-        <div className="TableData__head">Temp. Previsto</div>
-        <div className="TableData__head">Saldo</div>
+        <div className="TableData text-font-medium center-text">
+          <div className="TableData__head--data ">Data</div>
+          <div className="TableData__head--produto">Produto</div>
+          <div className="TableData__head">Un.</div>
+          <div className="TableData__head">Qtd.</div>
+          <div className="TableData__head">Qtd Prod.</div>
+          <div className="TableData__head tempo">Temp. Previsto</div>
+          <div className="TableData__head">Saldo</div>
+        </div>
+
+        <div className="dataTable">
+          {this.state.list
+            .sort(function (a, b) {
+              return (a.priority - b.priority) * -1;
+            })
+            .map((op) => {
+              return (
+                <Register
+                  data={op.data}
+                  produto={op.productId + " " + op.name}
+                  unidade={op.unit}
+                  quantidade={op.quantity}
+                  quantidadeProduto={op.productQuantity}
+                  tempo={op.tempo}
+                  saldo={op.balance}
+                  prioridade={op.priority}
+                />
+              );
+            })}
+        </div>
       </div>
-      <hr className="horizontalRule" />
-      <div className="dataTable">
-        {this.state.list.sort(function(a,b){return (a.priority - b.priority)*-1}).map((op)=>{
-          return(
-            <Register data={op.data} produto={op.productId+" "+op.name} unidade={op.unit}
-            quantidade={op.quantity} quantidadeProduto={op.productQuantity} tempo={op.tempo}
-            saldo={op.balance} prioridade={op.priority}
-            />
-          )
-        })}
-      </div>
-    </div>
-    )
+    );
   }
 }
