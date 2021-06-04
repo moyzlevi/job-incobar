0f5a594ba83fba3b619cc98c508e78001245e4b5
Moyses Pietsch <moyzlevi@gmail.com>
2021-06-04 15:47:39 -0300

    feat: added fetch to make request to an external api, added config

--- src/components/TableData/service.js
+++ src/components/TableData/service.js
@@ -1,9 +1,20 @@
-const fetch = require("node-fetch");
-import mockData from '../../../public/mock.json'
-
+import mockData from "../../../public/mock.json";
+import getURL from "../../../config.json"
 const getData = () => {
-    return mockData;
-   
+    const URL = getURL.endpoints.get;
+    let answer;
+  fetch(URL)
+    .then((data) => {
+      return data.json();
+    })
+    .then((res) => {
+      console.log(res,123);
+      answer = res;
+    })
+    .catch((error) => {
+      console.log("ERROR: fetch request failed!");
+    });
+  return answer;
 };
 
 export default getData;
