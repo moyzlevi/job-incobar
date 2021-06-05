2be6a8bdb94e6b468b2f7f372357198167034f71
Moyses Pietsch <moyzlevi@gmail.com>
2021-06-05 09:32:10 -0300

    fix: added fix to table data, now is fetching ok

--- src/components/TableData/TableData.jsx
+++ src/components/TableData/TableData.jsx
@@ -1,36 +1,54 @@
 import React, { Component } from "react";
 import "./TableData.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+import { Spinner } from "react-bootstrap";
 import Register from "../Register/Register";
 
-import getData from "./service.js";
+const axios = require("axios");
+const URL = "https://60baa10d42e1d0001761fe52.mockapi.io/api/v1/operations"
 
 export default class TableData extends Component {
+  _isMounted = false;
+
   constructor(props) {
     super(props);
-    this.state = { list: [] };
+    this.state = { list: [], isFetching: false};
     // this.refresh.bind = this.refresh.bind(this);
   }
 
-  async componentDidMount() {
-    this.interval = setInterval(() => this.refresh(), 60000);
-    this.refresh();
+   componentDidMount() {
+    this._isMounted = true;
+    this.fetchUsers();
+    this.timer = setInterval(() => this.fetchUsers(), 5000);
   }
+
+  fetchUsers() {
+    this.setState({ ...this.state, isFetching: true });
+    axios
+      .get(URL)
+      .then((response) => {
+        console.log("Responser",response.data[0].operations)
+        this.setState({ list: response.data[0].operations, isFetching: false });
+      })
+      .catch((e) => {
+        console.log(e);
+        this.setState({ ...this.state, isFetching: false });
+      });
+      console.log("LIST",this.state.list)
+  }
+
   componentWillUnmount() {
-    clearInterval(this.interval);
+    this._isMounted = false;
+    clearInterval(this.timer);
+    this.timer = null;
+
+
   }
-  
 
   printState() {
     let myState = this.state.list;
     console.log(myState);
-  }
-
-  refresh() {
-    let data = getData().operations;
-    this.setState({
-      list: data,
-    });
   }
 
   render() {
@@ -47,25 +65,32 @@ export default class TableData extends Component {
         </div>
 
         <div className="dataTable">
-          {this.state.list
-            .sort(function (a, b) {
-            
-              return (a.priority - b.priority) * -1 ||(a.sequencia - b.sequencia);
-            })
-            .map((op) => {
-              return (
-                <Register
-                  data={op.data}
-                  produto={op.productId + " " + op.name}
-                  unidade={op.unit}
-                  quantidade={op.quantity}
-                  quantidadeProduto={op.productQuantity}
-                  tempo={op.tempo}
-                  saldo={op.balance}
-                  prioridade={op.priority}
-                />
-              );
-            })}
+          {this.state.isFetching ? (
+            <div className="spindiv">
+              <Spinner className="spin" animation="border" />
+            </div>
+          ) : (
+            this.state.list
+              .sort(function (a, b) {
+                return (
+                  (a.priority - b.priority) * -1 || a.sequencia - b.sequencia
+                );
+              })
+              .map((op) => {
+                return (
+                  <Register
+                    data={op.data}
+                    produto={op.productId + " " + op.name}
+                    unidade={op.unit}
+                    quantidade={op.quantity}
+                    quantidadeProduto={op.productQuantity}
+                    tempo={op.tempo}
+                    saldo={op.balance}
+                    prioridade={op.priority}
+                  />
+                );
+              })
+          )}
         </div>
       </div>
     );
--- src/components/TableData/service.js
+++ src/components/TableData/service.js
@@ -1,20 +1,21 @@
 import mockData from "../../../public/mock.json";
 import getURL from "../../../config.json"
-const getData = () => {
+const getData = async () => {
     const URL = getURL.endpoints.get;
     let answer;
     console.log('Refresh...');
-  fetch(URL)
+  answer =  fetch(URL)
     .then((data) => {
       return data.json();
     })
     .then((res) => {
-      console.log(res,123);
-      answer = res;
+      
+      return res;
     })
     .catch((error) => {
       console.log("ERROR: fetch request failed!");
     });
+    console.log(answer,'AAA');
   return answer;
 };
 
